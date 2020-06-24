ffd25ea5cb38974caaf6560c8ef71e73247e251b
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-23 22:14:22 -0400

    Add resume link

--- components/Home.js
+++ components/Home.js
@@ -7,7 +7,9 @@ const Home = () => (
         <h2 className="side-text right">FOLIO</h2>
         <div className="profile-pic">
             <img src="/profile.jpg" alt="Profile Picture"/>
-            <p className="hero"> COMING SOON</p>
+            <div className="hero">COMING SOON
+            <a href="/CV_2A.pdf" target="_blank" className="resume">Resume</a>
+            </div>
         </div>
     </div>
 )
