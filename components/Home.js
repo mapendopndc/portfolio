80eda3682d38b849316b890aea361e67ae3cd790
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2020-06-24 00:31:47 -0400

    Add additional links

--- components/Home.js
+++ components/Home.js
@@ -7,8 +7,14 @@ const Home = () => (
         <h2 className="side-text right">FOLIO</h2>
         <div className="profile-pic">
             <img src="/profile.jpg" alt="Profile Picture"/>
-            <div className="hero">COMING SOON
-            <a href="/CV_2A.pdf" target="_blank" className="resume">Resume</a>
+            <div className="card">
+                <h3 className="hero">HELLO</h3>
+                <p className="about">My name is Mapendo Ngilinga de Carvalho, I'm a serial learner with a passion for software, design, and everything in between.</p>
+                <a href="/CV_2A.pdf" target="_blank" className="link">Resume</a>
+                <a href="/Portfolio_2A.pdf" target="_blank" className="link">Design</a>
+                <a href="https://www.linkedin.com/in/mapendopndc" target="_blank" className="link">LinkedIn</a>
+                <a href="https://github.com/mapendopndc" target="_blank" className="link">Github</a>
+                <a href="https://www.instagram.com/mapendo.ndc" target="_blank" className="link">Blog</a>
             </div>
         </div>
     </div>
