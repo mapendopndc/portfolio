23978af32792d404f97cce772c05f75c53447351
Mapendo Ngilinga de Carvalho <mapendopndc@gmail.com>
2021-07-19 02:50:17 -0400

    add and update portfolio

--- components/Home.js
+++ components/Home.js
@@ -14,7 +14,7 @@ const Home = () => (
                 <a href="https://www.linkedin.com/in/mapendopndc" target="_blank" className="link">LinkedIn</a>
                 <a href="https://github.com/mapendopndc" target="_blank" className="link">Github</a>
                 <a href="/Structural_Report.pdf" target="_blank" className="link">Engineering</a>
-                <a href="/Portfolio_2A.pdf" target="_blank" className="link">Design</a>
+                <a href="/Portfolio_3A.pdf" target="_blank" className="link">Design</a>
                 <a href="/Research.pdf" target="_blank" className="link">Research</a>
                 {/* <a href="https://www.instagram.com/mapendo.ndc" target="_blank" className="link">Photoblog</a> */}
             </div>
